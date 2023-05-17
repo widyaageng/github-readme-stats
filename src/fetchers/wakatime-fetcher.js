89d7e44780408b70d437a4644545d96b30265c42
Widya Ageng <39331919+widyaageng@users.noreply.github.com>
2023-05-18 01:01:20 +0800

    Update wakatime-fetcher.js
    Remove range, hardcode with all_time ie: https://wakatime.com/api/v1/users/widyaageng/stats/all_time?is_including_today=true

--- src/fetchers/wakatime-fetcher.js
+++ src/fetchers/wakatime-fetcher.js
@@ -7,10 +7,11 @@ import { MissingParamError } from "../common/utils.js";
  * @param {{username: string, api_domain: string, range: string}} props Fetcher props.
  * @returns {Promise<WakaTimeData>} WakaTime data response.
  */
-const fetchWakatimeStats = async ({ username, api_domain, range }) => {
+const fetchWakatimeStats = async ({ username, api_domain }) => {
   if (!username) throw new MissingParamError(["username"]);
 
   try {
+    const range = null
     const { data } = await axios.get(
       `https://${
         api_domain ? api_domain.replace(/\/$/gi, "") : "wakatime.com"
