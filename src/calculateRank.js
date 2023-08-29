8954edfba7c081c457945b8c2346066e395fa2e2
Widya Ageng <39331919+widyaageng@users.noreply.github.com>
2023-08-29 23:11:53 +0800

    yeah

--- src/calculateRank.js
+++ src/calculateRank.js
@@ -30,7 +30,7 @@ function calculateRank({
   stars,
   followers,
 }) {
-  const COMMITS_MEDIAN = all_commits ? 1000 : 250,
+  const COMMITS_MEDIAN = all_commits ? 750 : 250,
     COMMITS_WEIGHT = 4;
   const PRS_MEDIAN = 50,
     PRS_WEIGHT = 5;
